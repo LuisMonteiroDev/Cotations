93a9a5704c46a97affe7cba34bef0196679bc7ee
Luis Monteiro <felipemoreno9065@gmail.com>
2023-07-26 15:21:16 -0600

    Dashboard in dev

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import { useState, useEffect } from 'react';
 import { fetchCotationsData } from './services/axios';
+import { BarController } from 'chart.js';
 
 function App() {
   const [cotationsData, setCotationsData] = useState({});
+  const [chartData, setChartData] = useState({});
 
   useEffect(() => {
     const getCotationsData = async () => {
-      console.log('error');
       const data = await fetchCotationsData();
       setCotationsData(data);
     };
 
     getCotationsData();
   }, [])
+
+  useEffect(() => {
+    const createChartData = () => {
+      const labels = Object.keys(cotationsData);
+      const values = Object.values(cotationsData).map((currencyData) => currencyData.bid);
+
+      setChartData({
+        labels: labels,
+        datasets: [
+          {
+            label: 'Cotação',
+            data: values,
+            backgroundColor: 'rgba(54, 162, 235, 0.6)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+          }
+        ]
+      })
+    };
+    
+    if (Object.keys(cotationsData.length > 0)) {
+      createChartData();
+    }
+
+  }, [cotationsData])
+
   return (
     <div className="App">
       <h1>Cotações das moedas:</h1>
@@ -23,6 +50,7 @@ function App() {
         </p>
         ))}
       </div>
+      {/* <BarController data={chartData} options={{ scales: { y: { beginAtZero: true } } }} /> */}
     </div>
   );
 }
