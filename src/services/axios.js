58c0a2b5f497d73d271d4def1959034bab98f0d0
Luis Monteiro <felipemoreno9065@gmail.com>
2023-07-26 19:23:40 -0600

    Customo API

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,9 +2,26 @@ import axios from "axios";
 
 const URL = 'https://economia.awesomeapi.com.br'
 
-export const fetchCotationsData = async () => {
+export const fetchCotationsDataUSD = async () => {
     try{
-        const response = await axios.get(`${URL}/last/USD-BRL,EUR-BRL,BTC-BRL`);
+        const response = await axios.get(`${URL}/last/USD-BRL`);
+        return response.data;
+    } catch(error){
+        console.log('Erro ao obter os dados da API:', error);
+    };
+}
+
+export const fetchCotationsDataEUR = async () => {
+    try{
+        const response = await axios.get(`${URL}/last/EUR-BRL`);
+        return response.data;
+    } catch(error){
+        console.log('Erro ao obter os dados da API:', error);
+    };
+}
+export const fetchCotationsDataBTC = async () => {
+    try{
+        const response = await axios.get(`${URL}/last/BTC-BRL`);
         return response.data;
     } catch(error){
         console.log('Erro ao obter os dados da API:', error);
